e9e96d56ad8e128bebe9aad7742114199c3e9a84
Bozhidar Slaveykov <bobi@microweber.com>
2022-03-12 13:16:17 +0200

    Update wp_myfans.php

--- wp_myfans.php
+++ wp_myfans.php
@@ -1,11 +1,11 @@
 <?php
 /*
-Plugin Name: Wordpress MyFans
+Plugin Name: WP MyFans
 Plugin URI:
-Description:
+Description: This plugin helps to contect creators to make their own paid posts with monthly membership website.
 Version:  1.0.0
 Author: GeckoSystems
-Author URI: https://octolooks.com
+Author URI: https://geckosystems.com
 Text Domain: wp-myfans
 Domain Path: /languages
 */
