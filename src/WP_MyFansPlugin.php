061e8f15f901d257281d02a502767d92da1d701b
Bozhidar Slaveykov <bobi@microweber.com>
2022-03-12 14:05:04 +0200

    Update WP_MyFansPlugin.php

--- src/WP_MyFansPlugin.php
+++ src/WP_MyFansPlugin.php
@@ -1,4 +1,5 @@
 <?php
+
 namespace WP_MyFans;
 
 use WP_MyFans\Abstracts\WP_Plugin;
@@ -7,7 +8,25 @@ class WP_MyFansPlugin extends WP_Plugin
 {
 
 
+    public function run()
+    {
+        register_post_type("wp-myfans", array(
+            'labels' => array(
+                'name' => 'MyFans',
+                'all_items' => __('All posts', 'wp-myfans'),
+                'add_new' => __('Add new post', 'wp-myfans'),
+            ),
+            'public' => true,
+            'publicly_queriable' => true,
+            'show_ui' => true,
+            'menu_position' => 25,
+            'menu_icon' => '',
+           // 'supports' => array('custom-fields'), 
+            'has_archive' => true,
+            'rewrite' => false,
+            'capability_type' => 'post'
+        ));
+    }
 
-    
 
 }
