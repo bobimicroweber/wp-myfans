dfb891412d5c162cf81870c500cd6d5d47c9f761
Bozhidar Slaveykov <bobi@microweber.com>
2022-03-12 13:14:29 +0200

    update

---
+++ wp_myfans.php
@@ -0,0 +1,16 @@
+<?php
+/*
+Plugin Name: Wordpress MyFans
+Plugin URI:
+Description:
+Version:  1.0.0
+Author: GeckoSystems
+Author URI: https://octolooks.com
+Text Domain: wp-myfans
+Domain Path: /languages
+*/
+
+if (!defined('ABSPATH')) {
+    exit;
+}
+
