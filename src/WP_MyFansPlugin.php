85a6a99c22230366d94078e796012d36a0d65bf0
Bozhidar Slaveykov <bobi@microweber.com>
2022-03-12 14:29:28 +0200

    Update WP_MyFansPlugin.php

--- src/WP_MyFansPlugin.php
+++ src/WP_MyFansPlugin.php
@@ -10,22 +10,50 @@ class WP_MyFansPlugin extends WP_Plugin
 
     public function run()
     {
-        register_post_type("wp-myfans", array(
-            'labels' => array(
-                'name' => 'MyFans',
-                'all_items' => __('All posts', 'wp-myfans'),
-                'add_new' => __('Add new post', 'wp-myfans'),
-            ),
-            'public' => true,
-            'publicly_queriable' => true,
-            'show_ui' => true,
-            'menu_position' => 25,
-            'menu_icon' => '',
-           // 'supports' => array('custom-fields'), 
-            'has_archive' => true,
-            'rewrite' => false,
-            'capability_type' => 'post'
-        ));
+        $this->registerPostType();
+    }
+
+    function registerPostType()
+    {
+        register_post_type( 'wp-myfans', [
+            'label'  => null,
+            'labels' => [
+                'name'               => 'MyFans',
+                'menu_name'          => 'MyFans',
+                'all_items'          => __('All posts', 'wp-myfans'),
+                'singular_name'      => 'wp-myfans',
+                'add_new'            => __('Add new post', 'wp-myfans'),
+                'add_new_item'       => 'Add new item',
+                'edit_item'          => 'Edit post',
+                'new_item'           => 'New item',
+                'view_item'          => 'View Item',
+                'search_items'       => 'Search items',
+                'not_found'          => 'Not found',
+                'not_found_in_trash' => 'Not found in trash',
+                'parent_item_colon'  => '',
+            ],
+            'description'         => '',
+            'public'              => true,
+            // 'publicly_queryable'  => null,
+            // 'exclude_from_search' => null,
+            // 'show_ui'             => null,
+            // 'show_in_nav_menus'   => null,
+            'show_in_menu'        => null,
+            'show_in_admin_bar'   => true,
+            'show_in_rest'        => null,
+            'rest_base'           => null,
+            'menu_position'       => null,
+            'menu_icon'           => null,
+            //'capability_type'   => 'post',
+            //'capabilities'      => 'post',
+            //'map_meta_cap'      => null,
+            'hierarchical'        => true,
+            'supports'            => ['title','editor','author','thumbnail','excerpt','trackbacks','custom-fields','comments','revisions','page-attributes','post-formats'],
+            'taxonomies'          => [],
+            'has_archive'         => true,
+            'rewrite'             => false,
+            'query_var'           => true,
+        ] );
     }
 
 
