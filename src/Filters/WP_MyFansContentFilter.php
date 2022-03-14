3561efddf29cdd5809c03cf9143b9dbf3fc56b9a
Bozhidar Slaveykov <bobi@microweber.com>
2022-03-14 21:16:52 +0200

    update

--- src/Filters/WP_MyFansContentFilter.php
+++ src/Filters/WP_MyFansContentFilter.php
@@ -6,18 +6,17 @@ use WP_PluginFramework\Filters\WP_Filter;
 
 class WP_MyFansContentFilter extends WP_Filter {
 
-    public static function register() {
+    public static $filter = 'the_content';
 
-        add_filter('the_content', function ($content) {
+    public static function filter($data) {
 
-            global $wp_query;
+        global $wp_query;
 
-            // Check if we're inside the main loop in a single Post.
-            if (is_singular() && in_the_loop() && is_main_query()) {
-                var_dump(get_post());
-            }
+        // Check if we're inside the main loop in a single Post.
+        if (is_singular() && in_the_loop() && is_main_query()) {
+            var_dump(get_post());
+        }
 
-            return $content;
-        }, 5);
+        return $data;
     }
 }
--- vendor/geckosystems/wp-pluginframework/src/Filters/WP_Filter.php
+++ vendor/geckosystems/wp-pluginframework/src/Filters/WP_Filter.php
@@ -3,7 +3,18 @@ namespace WP_PluginFramework\Filters;
 
 abstract class WP_Filter
 {
-    public static function register() {
+    public static $filter;
+
+    public static function register()
+    {
+
+        add_filter(static::$filter, function ($data) {
+            return static::filter($data);
+        }, 5);
+    }
+
+    public static function filter($data)
+    {
 
     }
 }
