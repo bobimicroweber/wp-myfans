f1ba833739af69729b3331e5e9a863a8b6bed9d9
Bozhidar Slaveykov <bobi@microweber.com>
2022-03-14 19:20:41 +0200

    Update WP_MyFansPlugin.php

--- src/WP_MyFansPlugin.php
+++ src/WP_MyFansPlugin.php
@@ -16,7 +16,7 @@ class WP_MyFansPlugin extends WP_Plugin
             global $wp_query;
 
             // Check if we're inside the main loop in a single Post.
-            if ( is_singular() && in_the_loop() && is_main_query() ) {
+            if (is_singular() && in_the_loop() && is_main_query()) {
                 var_dump(get_post());
             }
 
@@ -24,41 +24,44 @@ class WP_MyFansPlugin extends WP_Plugin
         }, 5);
 
 
-
         $supports = array('title', 'editor', 'author', 'thumbnail', 'comments', 'revisions', 'page-attributes', 'post-formats');
 
         $labels = array(
-            'name'          => _x('MyFans Posts', 'plural'),
+            'name' => _x('MyFans Posts', 'plural'),
             'singular_name' => _x('Posts', 'singular'),
-            'menu_name'     => _x('MyFans', 'admin menu'),
-            'name_admin_bar'=> _x('Posts', 'admin bar'),
-            'add_new'       => _x('Add New', 'add new'),
-            'add_new_item'  => __('Add New Post'),
-            'new_item'      => __('New Post'),
-            'edit_item'     => __('Edit Post'),
-            'view_item'     => __('View Post'),
-            'all_items'     => __('All Posts'),
-            'search_items'  => __('Search Posts'),
-            'not_found'     => __('No Posts found.')
+            'menu_name' => _x('MyFans', 'admin menu'),
+            'name_admin_bar' => _x('Posts', 'admin bar'),
+            'add_new' => _x('Add New', 'add new'),
+            'add_new_item' => __('Add New Post'),
+            'new_item' => __('New Post'),
+            'edit_item' => __('Edit Post'),
+            'view_item' => __('View Post'),
+            'all_items' => __('All Posts'),
+            'search_items' => __('Search Posts'),
+            'not_found' => __('No Posts found.')
         );
 
         $args = array(
-            'supports'      => $supports,
-            'labels'        => $labels,
-            'public'        => true,
-            'query_var'     => true,
-            'rewrite'       => array('slug' => 'posts'),
+            'supports' => $supports,
+            'labels' => $labels,
+            'public' => true,
+            'query_var' => true,
+            'rewrite' => array('slug' => 'posts'),
             // 'taxonomies'    => [],
-            'has_archive'   => true,
-            'hierarchical'  => false
+            'has_archive' => true,
+            'hierarchical' => false
         );
         $this->registerPostType('my-fans', $args);
 
 
+        add_action("add_meta_boxes", function () {
+            add_meta_box('wp_myfans_post_meta', __('Require Membership'), function () {
 
-        add_action( "add_meta_boxes", function () {
-            add_meta_box("newsletter_main_post",
-                esc_html("Add article", "newsletter"), array($this, "add_article_html"), "newsletter", 'advanced');
+                echo 'dadada';
+                
+
+            }, 'my-fans', 'side', 'high');
+
         });
     }
 
